0c8239442ff8e570926cfa66a9c48ff4189bffe5
squalonero <mirco.cucca@gmail.com>
2022-06-11 19:49:30 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,39 @@
 <template>
   <div id="Home">
-      <h1>Home</h1>
+    <!-- <select name="hero" id="hero" v-model="selectedHero">
+      <option value="" selected>Seleziona un eroe</option>
+      <option v-for="(hero, k) in hStore.heroes" :key="k">{{ hero.name }}</option>
+    </select> -->
+    <div class="list-wrap">
+      <div class="form-group" v-for="(hero, k) in hStore.heroes" :key="k">
+        {{ hero.name }}
+      </div>
+    </div>
   </div>
 </template>
 <script>
-export default {
-    name: 'Home',
-    setup() {
+import { useHeroStore } from '@/store/heroes.js'
 
-    },
+export default {
+  name: 'Home',
+  data() {
+    return {
+      selectedHero: ''
+    }
+  },
+  setup() {
+    const hStore = useHeroStore()
+    return {
+      // you can return the whole store instance to use it in the template
+      hStore
+    }
+  }
 }
 </script>
+<style scoped>
+.list-wrap
+{
+  overflow-y: auto;
+  max-height: 100%;
+}
+</style>
